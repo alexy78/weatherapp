5ce35ece0ddcc7d955541a23d8ce2f5ba8547238
alexy78 <sashapeshehonova@gmail.com>
2022-07-05 22:47:54 +0000

    cityvalue

--- java.js
+++ java.js
@@ -37,41 +37,9 @@ if (minutes < 10) {
 }
 h3.innerHTML = `${currentDay}, ${currentMonth} ${currentDate}, ${currentYear}, ${hours}:${minutes}`;
 
-function displayTemp(response) {
-  let currentTemp = document.querySelector();
-}
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-
-  celsiusTemperature = response.data.main.temp;
-
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-}
-
-function search(city) {
-  let apiKey = "be3ab8e09c2856abe3ab8e09c2856ab67e7aa09558d9610";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
-}
-
-function handleSubmit(event) {
+function currentCity(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("#cityinput");
-  search(cityInputElement.value);
+  let searchCity = document.querySelector("#change-city");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = searchCity.value;
 }
